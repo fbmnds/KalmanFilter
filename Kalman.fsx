d326d04e4d9db53cdad505084831c5a28be2c4e5
fbmnds <fbmnds7@gmail.com>
2014-03-08 18:41:44 +0100

    initial commit

--- Kalman.fsx
+++ Kalman.fsx
@@ -63,14 +63,14 @@ let randomTrend1 = [for i in 0.0 .. 0.1 .. 10.0 -> i, sin i + rand()]
 let randomTrend2 = [for i in 0.0 .. 0.1 .. 10.0 -> i, sin i * cos i + rand()]
 
 let randomKF = [for i in 0 .. 100 -> applyKalmanFilter<m,s> kf (1.0<m>*(snd randomTrend1.[i])) (1.0<m/s>*(snd randomTrend2.[i])) ]
-let randomKF_x = [for i in 0 .. 100 -> (float i)/10., (max (float randomKF.[i].x) 1.0)]
-let randomKF_xdot = [for i in 0 .. 100 -> (float i)/10., (max (float randomKF.[i].xdot) 1.0)]
+let randomKF_x = [for i in 0 .. 100 -> (float i)/10., (max (float randomKF.[i].x) 1.2)]
+let randomKF_xdot = [for i in 0 .. 100 -> (float i)/10., (max (float randomKF.[i].xdot) 1.2)]
 
 
 #load "FSharpChart.fsx"
 open MSDN.FSharp.Charting
 
 type Chart = FSharpChart
-Chart.Combine [Chart.Line randomTrend1; Chart.Point randomTrend2; Chart.Point randomKF_x; Chart.Point randomKF_xdot ]
+Chart.Combine [Chart.Line randomTrend1; Chart.Line randomTrend2; Chart.Line randomKF_x; Chart.Line randomKF_xdot ]
 
 
